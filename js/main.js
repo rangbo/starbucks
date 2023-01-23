e3ea6fd76775487cc2eb5be4f7856ef87053c47d
rangbo <partypeeoplle@naver.com>
2023-01-23 13:00:10 +0900

    promotion, notice

--- js/main.js
+++ js/main.js
@@ -31,3 +31,46 @@ window.addEventListener('scroll', _.throttle(function(){
         });
     }
 }, 300));
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+    gsap.to(fadeEl, 1, {
+        delay:(index + 1) * .7,
+        opacity:1
+    });
+});
+
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
+new Swiper('.promotion .swiper-container', {
+    autoplay:{
+        delay:5000
+    },
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides:true,
+    loop:true,
+    pagination:{
+        el:'.promotion .swiper-pagination',
+        clickable: true
+    },
+    navigation:{
+        prevEl:'.promotion .swiper-prev',
+        nextEl:'.promotion .swiper-next'
+    }
+});
+
+const promotionEl = document.querySelector('.promotion')
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false
+promotionToggleBtn.addEventListener('click', function () {
+    isHidePromotion = !isHidePromotion
+    if (isHidePromotion) {
+        promotionEl.classList.add('hide')
+    } else {
+        promotionEl.classList.remove('hide')
+    }
+})
